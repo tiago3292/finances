25f1ceee1b8e50f88a373b363488c3b374801087
tiago3292 <tiago_santos333@hotmail.com>
2026-07-16 15:13:42 -0300

    Criação de User models e User schemas

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,11 @@
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base import Base
 
-#metadata = Base.metadata
-
-# Classe de teste
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(60), unique=True)
-    phone = Column(Integer, nullable=True)
-    is_admin = Column(Boolean, default=False)
-
-    def __repr__(self) -> str:
-        return f"id: {self.id}, name: {self.name}, is_admin {self.is_admin}"
+    # mapped_column() só é necessário para declarar a primary key
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    email: Mapped[str]
+    password: Mapped[int]
---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    model_config = {"from_attributes": True}
+
+class UserInDB(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    hashed_password: str
+    model_config = {"from_attributes": True}
